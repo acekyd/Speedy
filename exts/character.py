c57b3da7b147ec9201f0ef624fab546fd358234e
Jimmy-Blue <60958064+Jimmy-Blue@users.noreply.github.com>
2022-12-03 22:42:50 +0700

    feat: Add button check for items

--- exts/character.py
+++ exts/character.py
@@ -2,6 +2,7 @@ import logging
 import datetime
 import json
 import interactions
+from interactions.ext.wait_for import wait_for_component
 
 
 def get_color(char_class: str):
@@ -14,25 +15,54 @@ def get_color(char_class: str):
     :rtype: str
     """
     match char_class:
-        case 'Common':
+        case "Common":
             return 0xc0dcfc
+        case "Rare":
+            return 0xf6e094
+        case "Super Rare":
+            return 0xd9b3ff
+        case "Special":
+            return 0x92f190
 
 
-def create_bar(num: int) -> str:
+def create_bar(stat: str, num: int) -> str:
 
     green_bar: str = "<:bar_green:1045317811926466650>"
     red_bar: str = "<:bar_red:1045317809997090896>"
     bar_orange: str = "<:bar_orange:1045317805790212176>"
     bar_empty: str = "<:bar_empty:1045317807820242994>"
 
-    if 1 <= num <= 5:
-        bar = num * red_bar
-        return f"{bar}" + ((10 - num) * f"{bar_empty}")
-    elif 6 <= num <= 9:
-        bar = num * bar_orange
-        return f"{bar}" + ((10 - num) * f"{bar_empty}")
-    elif num == 10:
-        return 10 * f"{green_bar}"
+    match stat:
+        case "speed":
+            if 1 <= num <= 3:
+                bar = num * red_bar
+                return f"{bar}" + ((10 - num) * f"{bar_empty}")
+            elif 4 <= num <= 7:
+                bar = num * bar_orange
+                return f"{bar}" + ((10 - num) * f"{bar_empty}")
+            elif 8 <= num <= 10:
+                bar = num * f"{green_bar}"
+                return f"{bar}" + ((10 - num) * f"{bar_empty}")
+        case "acceleration":
+            if 1 <= num <= 3:
+                bar = num * red_bar
+                return f"{bar}" + ((10 - num) * f"{bar_empty}")
+            elif 4 <= num <= 8:
+                bar = num * bar_orange
+                return f"{bar}" + ((10 - num) * f"{bar_empty}")
+            elif 9 <= num <= 10:
+                bar = num * f"{green_bar}"
+                return f"{bar}" + ((10 - num) * f"{bar_empty}")
+        case "strength":
+            if 1 <= num <= 3:
+                bar = num * red_bar
+                return f"{bar}" + ((10 - num) * f"{bar_empty}")
+            elif 4 <= num <= 7:
+                bar = num * bar_orange
+                return f"{bar}" + ((10 - num) * f"{bar_empty}")
+            elif 8 <= num <= 10:
+                bar = num * f"{green_bar}"
+                return f"{bar}" + ((10 - num) * f"{bar_empty}")
 
 
 class Character(interactions.Extension):
@@ -49,26 +79,26 @@ class Character(interactions.Extension):
     async def _character(self, ctx: interactions.CommandContext, character_name: str):
         """Usage: /character [character_name]"""
         name_lower = character_name.lower()
-        db = json.loads(open("./db/character.json", "r", encoding="utf8").read())
-        if name_lower not in db:
+        char_db = json.loads(open("./db/character.json", "r", encoding="utf8").read())
+        if name_lower not in char_db:
             return await ctx.send("Character not found.", ephemeral=True)
 
-        name = db[name_lower]["name"]
-        color = get_color(db[name_lower]["rarity"])
-        speed = db[name_lower]["speed"]
-        acceleration = db[name_lower]["acceleration"]
-        strength = db[name_lower]["strength"]
-        rarity = db[name_lower]["rarity"]
-        max_starting_rings = db[name_lower]["max_starting_rings"]
-        image = db[name_lower]["image"]
+        name = char_db[name_lower]["name"]
+        color = get_color(char_db[name_lower]["rarity"])
+        speed = char_db[name_lower]["speed"]
+        acceleration = char_db[name_lower]["acceleration"]
+        strength = char_db[name_lower]["strength"]
+        rarity = char_db[name_lower]["rarity"]
+        max_starting_rings = char_db[name_lower]["max_starting_rings"]
+        image = char_db[name_lower]["image"]
 
         items_button = [
             interactions.Button(
                 style=interactions.ButtonStyle.SECONDARY,
                 label=item,
-                custom_id=item.lower().replace(" ", "_")
+                custom_id=item
             )
-            for item in db[name_lower]['items']
+            for item in char_db[name_lower]['items']
         ]
 
         embed = interactions.Embed(
@@ -76,17 +106,33 @@ class Character(interactions.Extension):
             description="".join(
                 [
                     f"Rarity: {rarity}\n",
-                    f"Max Starting Rings: {max_starting_rings}",
+                    f"Max Starting Rings: {max_starting_rings}\n",
+                    f"Total Stats: {speed + acceleration + strength}"
                 ]
             ),
             color=color,
             thumbnail=interactions.EmbedImageStruct(url=image)
         )
-        embed.add_field(name=f"Speed: {speed}/10", value=create_bar(speed))
-        embed.add_field(name=f"Acceleration: {acceleration}/10", value=create_bar(acceleration))
-        embed.add_field(name=f"Strength: {strength}/10", value=create_bar(strength))
+        embed.add_field(name=f"Speed: {speed}/10", value=create_bar("speed", speed))
+        embed.add_field(name=f"Acceleration: {acceleration}/10", value=create_bar("acceleration", acceleration))
+        embed.add_field(name=f"Strength: {strength}/10", value=create_bar("strength", strength))
 
         await ctx.send(embeds=embed, components=items_button)
+
+        while True:
+            res = await wait_for_component(
+                self.client,
+                components=items_button,
+                messages=int(ctx.message.id),
+                timeout=45,
+            )
+
+            projectiles_db = json.loads(open("./db/items-projectile.json", "r", encoding="utf8").read())
+
+            if res.custom_id in projectiles_db:
+                await res.send(f"{projectiles_db[res.custom_id]}")
+
+            await res.send(f"Clicked! ID: {res.custom_id}")
 
     @interactions.extension_autocomplete(command="character", name="character_name")
     async def auto_complete(
